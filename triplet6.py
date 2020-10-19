e1a35eb345a705308cccd8ab67bd38bc43c2ba77
SUMIN97 <julia4034280@naver.com>
2020-10-19 15:19:25 +0900

    plot 완료

--- triplet6.py
+++ triplet6.py
@@ -16,13 +16,18 @@ from glob import glob
 from torch.autograd import Variable
 
 
+import matplotlib
+import matplotlib.pyplot as plt
+
+
 torch.cuda.empty_cache()
 
 class TripletDataset(Dataset):
     def __init__(self, folder_path, transform, folders):
         self.transform = transform
         self.folder_path = folder_path
-    
+        self.style = self.folder_path.split('/')[-1].split('_')[0]
+        self.content = self.folder_path.split('/')[-1].split('_')[1]
         self.img_list = os.listdir(self.folder_path)
         self.folders = folders
 
@@ -30,10 +35,15 @@ class TripletDataset(Dataset):
     def __getitem__(self, index):
         anchor = self.read_image(os.path.join(self.folder_path, self.img_list[index]))
 
-        positive_index = index
-        while positive_index == index:
-            positive_index = np.random.randint(len(self.img_list))
-        posive = self.read_image(os.path.join(self.folder_path, self.img_list[positive_index]))
+        while True:
+            positive_path = self.folders[np.random.randint(len(self.folders))]
+            p_style = positive_path.split('/')[-1].split('_')[0]
+            p_content = positive_path.split('/')[-1].split('_')[1]
+            if p_style == self.style or p_content == self.content: break
+
+        positive_list = os.listdir(positive_path)
+        positive_index = np.random.randint(len(positive_list))
+        positive = self.read_image(os.path.join(positive_path, positive_list[positive_index]))
 
         negative_path = self.folder_path
         while negative_path == self.folder_path:
@@ -43,7 +53,7 @@ class TripletDataset(Dataset):
         negative_index = np.random.randint(len(negative_list))
         negative = self.read_image(os.path.join(negative_path, negative_list[negative_index]))
 
-        return anchor, posive, negative
+        return anchor, positive, negative
 
 
     def read_image(self, path):
@@ -161,7 +171,8 @@ class StyleNet(nn.Module):
     def forward(self, x):
         output = self.conv(x)
         output = self.gram_and_flatten(output)
-        output = self.PCA_svd(output, n_components, True)
+        # output = self.PCA_svd(output, n_components, True)
+        output = self.PCA_svd(output, 1, True) #좌
         return output
     """
     def forward(self, x):
@@ -206,7 +217,8 @@ class ContentNet(nn.Module):
             nn.MaxPool2d(2, 2)
         )
         self.avg_pool = nn.AvgPool2d(7)
-        self.fc1 = nn.Linear(512, n_components)
+        # self.fc1 = nn.Linear(512, n_components)
+        self.fc1 = nn.Linear(512, 1) #y좌표
 
     def forward(self, x):
         # output1 = self.convnet(x)
@@ -218,16 +230,20 @@ class ContentNet(nn.Module):
         #     print("input", x, "output1", output1, "output2", output2, "output3", output3, "output4", output4)
         return output3
 
+
+
 use_cuda = torch.cuda.is_available()
 #kwargs = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
 margin = 1
 lr = 1e-4
-n_epochs = 30
-n_components = 8#32
+n_epochs = 10
+
+n_components = 16#32
 batch_size = n_components
 
+
 # cuda = False
-device = torch.device("cuda:0" if use_cuda else "cpu")
+device = torch.device("cuda:1" if use_cuda else "cpu")
 # os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2, 3'
 print("use cuda", use_cuda,"device", device)
 
@@ -251,11 +267,11 @@ transform = transforms.Compose([
 
 
 for epoch in range(n_epochs):
-    print("epoch", epoch)
     #Train
-    folders = glob(os.path.join('/home/lab/Documents/SWMaestro/AWS/Bam_Triplet/BAM', '*'))
+    folders = sorted(glob(os.path.join('/home/lab/Documents/ssd/SWMaestro/test', '*')))
     # folders = glob(os.path.join('/home/ubuntu/artri/Bam_Triplet/BAM', '*'))
-    
+    total_correct = 0
+
     for folder in folders:
         dataset = TripletDataset(folder, transform, folders)
         loader = DataLoader(dataset, batch_size = batch_size, shuffle= True)
@@ -284,18 +300,13 @@ for epoch in range(n_epochs):
 
             dis_pos = (anchor - positive).pow(2).sum(dim = 1).pow(.5)
             dis_neg = (anchor - negative).pow(2).sum(dim = 1).pow(.5)
-            b = dis_pos.size()
-            losses = torch.max((dis_pos - dis_neg + margin), torch.zeros(b).double().cuda())
+            b = dis_pos.size()[0]
 
-            count= (losses == torch.zeros(b).double().cuda()).sum().item()
-            print(count)
+            losses = F.relu(dis_pos - dis_neg + margin)
+            count = (losses == torch.zeros(b).cuda()).sum()
             correct += count
-
-            style_opimizer.zero_grad()
-            content_opimizer.zero_grad()
-            sum_losses = losses.sum()
-            sum_losses.backward()
-
+            losses_sum = losses.sum()
+            losses_sum.backward()
             is_okay_to_optimize = True
             for param in style_model.parameters():
                 a = param.grad.view(-1)[:10]
@@ -312,7 +323,106 @@ for epoch in range(n_epochs):
             if is_okay_to_optimize:
                 style_opimizer.step()
                 content_opimizer.step()
-        print("correct", correct)
+
+        percent = correct / len(dataset) * 100
+        print("correct: {}/{} ({}%)".format(correct, len(dataset), str(percent)))
+        total_correct += correct
+    print("epoch", epoch, 'total_correct', total_correct)
+
+# colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f','#bcbd22',
+#           '#17becf', '#C0392B ', '#E74C3C ', '#9B59B6 ', '#8E44AD ', '#2980B9 ', '#3498DB ', '#1ABC9C', '#16A085 ',
+#             '#27AE60', '#2ECC71 ', '#F1C40F', '#F39C12', '#E67E22', '#D35400 ', '#BDC3C7','#839192 ', '#34495E ',
+#             '#2C3E50', '#FE2EF7', '#F5A9A9', '#F5D0A9', '#BCF5A9', '#A9F5F2', '#A9BCF5','#BCA9F5', '#D7DF01',
+#             '#DF0101', '#088A68', '#3B240B', '#610B21', '#BF00FF', '#08298A', '#DF01A5','#F781BE', '#58FA58',
+#             '#5F04B4', '#0489B1', '#6E6E6E', '#000000', '#2E2E2E', '#210B61', '#FA8258','#FF4000', '#DF7401',
+#             '#F7BE81', '#F2F5A9', '#58FAD0', '#A9F5BC', '#CD5C5C', '#DC143C', '	#FF6347','#FFD700', '#BDB76B	',]
+
+# https://html-color-codes.info/    content 가 같으면 같은색
+# Bicycle : 빨
+# bird : 주
+# building :노
+#cars: 초
+# cat: 민트
+# dog: 파
+# flower: 보
+# people:분홍
+# tree : 회
+#
+# 명도: (흰) 3dgraphic > comic > oil > pen > pencil > vectorart > watercolor
+
+# rgba_color = np.array(
+#     [255, 0, 0, 0.2],   [255, 112, 0, 0.2], [255, 255, 0, 0.2], [0, 255, 0, 0.2], [0, 255, 199, 0.2], [0, 0, 255, 0.2], [138, 0, 255, 0.2], [249, 0, 255, 0.2], [21, 22, 14, 0.2],
+#     [255, 0, 0, 0.3],   [255, 112, 0, 0.3], [255, 255, 0, 0.3], [0, 255, 0, 0.3], [0, 255, 199, 0.3], [0, 0, 255, 0.3] [138, 0, 255, 0.3], [249, 0, 255, 0.3], [21, 22, 14, 0.3],
+#     [255, 0, 0, 0.4],   [255, 112, 0, 0.4], [255, 255, 0, 0.4], [0, 255, 0, 0.4], [0, 255, 199, 0.4], [0, 0, 255, 0.4], [138, 0, 255, 0.4], [249, 0, 255, 0.4], [21, 22, 14, 0.4],
+#     [255, 0, 0, 0.6],   [255, 112, 0, 0.6], [255, 255, 0, 0.6], [0, 255, 0, 0.6], [0, 255, 199, 0.6], [0, 0, 255, 0.6], [138, 0, 255, 0.6], [249, 0, 255, 0.6], [21, 22, 14, 0.6],
+#     [255, 0, 0, 0.7],   [255, 112, 0, 0.7], [255, 255, 0, 0.7], [0, 255, 0, 0.7], [0, 255, 199, 0.7], [0, 0, 255, 0.7], [138, 0, 255, 0.7], [249, 0, 255, 0.7], [21, 22, 14, 0.7],
+#     [255, 0, 0, 0.8],   [255, 112, 0, 0.8], [255, 255, 0, 0.8], [0, 255, 0, 0.8], [0, 255, 199, 0.8], [0, 0, 255, 0.8], [138, 0, 255, 0.8], [249, 0, 255, 0.8], [21, 22, 14, 0.8],
+#     [255, 0, 0, 1.0],   [255, 112, 0, 1], [255, 255, 0, 1.0], [0, 255, 0, 1.0], [0, 255, 199, 1.0], [0, 0, 255, 1.0], [138, 0, 255, 1.0], [249, 0, 255, 1.0], [21, 22, 14, 1.0]
+# )
+
+
+#9로 나눈 몫
+alpha_dict = {
+    0: 0.2,
+    1: 0.3,
+    2: 0.4,
+    3:0.6,
+    4:0.7,
+    5:0.8,
+    6:1.0
+}
+
+#9로 나눈 나머
+color_dict = {
+    0: np.array([255, 0, 0]),
+    1: np.array([255, 112, 0]),
+    2:  np.array([255, 255, 0]),
+    3: np.array([0, 255, 0]),
+    4: np.array([0, 255, 199]),
+    5: np.array([0, 0, 255]),
+    6: np.array([138, 0, 255]),
+    7:np.array([249, 0, 255]),
+    8:np.array([21, 22, 14])
+}
+
+plt.figure(figsize=(20,20))
+xmin  = ymin = 100000000000000
+xmax = ymax = -1000000000000
+with torch.no_grad():
+    style_model.eval()
+    content_model.eval()
+
+    folders = glob(os.path.join('/home/lab/Documents/ssd/SWMaestro/test', '*'))
+    for idx, folder in enumerate(folders):
+        dataset = TripletDataset(folder, transform, folders)
+        loader = DataLoader(dataset, batch_size = batch_size, shuffle= True)
+
+
+
+        correct = 0
+        for batch_idx, (anchor, positive, negative) in enumerate(tqdm(loader)):
+            torch.cuda.empty_cache()
+            if use_cuda:
+                anchor = anchor.cuda()
+
+            b = anchor.size()[0]
+
+            x = style_model(anchor).double().cpu()
+            y = content_model(anchor).double().cpu()
+
+            if (x > xmax): xmax = x
+            if (x < xmin): xmin = x
+            if (y > ymax): ymax = y
+            if (y < ymin): ymin = y
+
+            for i in range(b):
+                plt.scatter(x[i], y[i], s = 20,color = color_dict[idx %9]/255, alpha = alpha_dict[int(idx/9)])
+    plt.axis([xmin, xmax, ymin, ymax])
+    plt.show()
+    plt.savefig("result1.png")
+
+
+
 
 
 
