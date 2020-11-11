815a105c8f543e8fb32e99960982f6e2ec8a0c37
SUMIN97 <julia4034280@naver.com>
2020-11-11 15:07:11 +0900

    model update

--- test.py
+++ test.py
@@ -19,10 +19,11 @@ torch.cuda.empty_cache()
 
 
 class TripletDataset(Dataset):
-    def __init__(self, transform, imgs_path):
-        self.transform = transform
+    def __init__(self, imgs_path, style_transform):
+
         self.imgs_path = imgs_path
         self.n_imgs = len(self.imgs_path)
+        self.style_transform = style_transform
 
     def __getitem__(self, index):
 
@@ -41,19 +42,35 @@ class TripletDataset(Dataset):
             negative_path = self.imgs_path[np.random.randint(self.n_imgs)]
             n_material = negative_path.split('/')[-1].split('_')[0]
             n_author = negative_path.split('/')[-1].split('_')[1]
-            if (n_material != anchor_material) or (n_author != anchor_author):
+            if (n_material != anchor_material) and (n_author != anchor_author):
                 break
 
-        anchor = self.read_image(anchor_path)
-        positive = self.read_image(positive_path)
-        negative = self.read_image(negative_path)
+        anchor_rgb = self.read_image_rgb(anchor_path) #[224, 224, 3]
+        anchor_style = self.read_image_style(anchor_path) #[3, 224, 224]
+        # print("anchor style size", anchor_style.size())
+        # print("anchor_rgb size", anchor_rgb.size())
 
-        return anchor, positive, negative
 
-    def read_image(self, path):
+        positive_rgb = self.read_image_rgb(positive_path)
+        positive_style = self.read_image_style(positive_path)
+
+        negative_rgb = self.read_image_rgb(negative_path)
+        negative_style = self.read_image_style(negative_path)
+
+
+        return anchor_style, anchor_rgb, positive_style, positive_rgb, negative_style, negative_rgb
+
+    def read_image_rgb(self, path):
         image = Image.open(path)
         image = image.convert('RGB')
-        image = self.transform(image)
+        image = np.array(image)
+        image = torch.tensor(image)
+        return image
+
+    def read_image_style(self, path):
+        image = Image.open(path)
+        image = image.convert('RGB')
+        image = self.style_transform(image)
         return image
 
     def __len__(self):
@@ -64,7 +81,7 @@ class StyleNet(nn.Module):
     def __init__(self):
         super(StyleNet, self).__init__()
         conv_list = list(vgg16(pretrained=True).features)[:-2]
-        conv_list[-1] = nn.Conv2d(512, 32, 3, padding=1)
+        # conv_list[-1] = nn.Conv2d(512, 32, 3, padding=1)
         self.convnet = nn.Sequential(*conv_list)
 
     def gram_and_flatten(self, x):
@@ -73,46 +90,43 @@ class StyleNet(nn.Module):
         mul = torch.bmm(feature, feature.transpose(1, 2))
         return mul.view(batch, -1)  # (batch, 512 * 512)
 
-    def sumin_pca(self, X, k):
-
-        u, s, v = torch.pca_lowrank(X, center=True)
-        return torch.mm(X, v[:, :k])
-
-    def PCA_svd(self, X, k, center=True):
-        n = X.size()[0]
-        ones = torch.ones(n).view([n, 1])
-        h = (1 / n) * torch.mm(ones, ones.t()) if center else torch.zeros(n * n).view([n, n])
-        H = torch.eye(n) - h
-        X_center = torch.mm(H.double().to(device), X.double())
-
-        try:
-            u, s, v = torch.svd(X_center)
-        except:
-
-            u, s, v = torch.svd(X_center + 1e-4 * X_center.mean())
-        x_new = torch.mm(X, v[:, :k].float())
-        return x_new
 
     def forward(self, x):
         output = self.convnet(x)
         output = self.gram_and_flatten(output)
-        output = self.PCA_svd(output, n_components)
+        # output = self.PCA_svd(output, n_components)
         return output
 
 
-class ContentNet(nn.Module):
-    def __init__(self):
-        super(ContentNet, self).__init__()
-        self.convnet = nn.Sequential(*list(vgg16(pretrained=True).features))
-        self.avg_pool = nn.AvgPool2d(7)
-        self.fc1 = nn.Linear(512, n_components)
 
-    def forward(self, x):
-        output1 = self.convnet(x)
-        output2 = self.avg_pool(output1)
-        output2 = output2.view(-1, 512)
-        output3 = self.fc1(output2)
-        return output3
+class RGB3D(nn.Module):
+    def __init__(self):
+        super(RGB3D, self).__init__()
+        self.term = 16
+        self.n = int((255 - 0 +1)/self.term +1)
+
+    def forward(self,imgs):
+        batch, height, width, color= imgs.size()
+
+        self.r = torch.zeros(batch, self.n).to(device)
+        self.g = torch.zeros(batch, self.n).to(device)
+        self.b = torch.zeros(batch, self.n).to(device)
+
+
+        for b in range(batch):
+            for i in range(625):
+                x = np.random.randint(height)
+                y = np.random.randint(width)
+                self.r[b][int(imgs[b][x][y][0]//self.term)] +=1
+                self.g[b][int(imgs[b][x][y][1]//self.term)] +=1
+                self.b[b][int(imgs[b][x][y][2] // self.term)] += 1
+
+        self.colorspace = torch.cat((self.r, self.g, self.b), dim = 1)
+        return self.colorspace
+
+
+
+
 
 
 use_cuda = torch.cuda.is_available()
@@ -120,17 +134,15 @@ use_cuda = torch.cuda.is_available()
 margin = 0.
 lr = 1e-3
 n_epochs = 50
-n_components = 16
-batch_size = 16
+n_components = 8
+batch_size = 8
 
 device = torch.device("cuda:1" if use_cuda else "cpu")
 print("use cuda", use_cuda, "device", device)
 
 style_model = StyleNet()
-content_model = ContentNet()
+rgb_model = RGB3D()
 
-# print(style_model)
-# print(content_model)
 
 for idx, param in enumerate(style_model.parameters()):
     print("style", idx, param.size())
@@ -139,47 +151,45 @@ for idx, param in enumerate(style_model.parameters()):
 
 if use_cuda:
     style_model.to(device)
-    content_model.to(device)
+    rgb_model.to(device)
+
 
 style_optimizer = optim.Adam(list(style_model.parameters()), lr=lr)
-content_optimizer = optim.Adam(list(content_model.parameters()), lr=lr)
 
-transform = transforms.Compose([
+
+style_transform = transforms.Compose([
     # transforms.Resize([224, 224]),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # 0~1값을 -0.5~0.5로 변경
 ])
 
+
+
+
 cos = nn.CosineSimilarity(dim=1, eps=1e-6)
 
-# folders = glob(os.path.join('/home/lab/Documents/ssd/SWMaestro/Grapolio/동양화/', '*', '*'))
-folders = glob(os.path.join('/home/lab/Documents/ssd/SWMaestro/Grapolio_Resize_224', '*'))
 
+imgs_path = glob(os.path.join('/home/lab/Documents/ssd/SWMaestro/Grapolio_Resize_224', '*'))
 
-dataset = TripletDataset(transform, folders)
+dataset = TripletDataset(imgs_path, style_transform)
 loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 
 
 for epoch in range(n_epochs):
     correct = 0
-    for batch_idx, (anchor, positive, negative) in enumerate(tqdm(loader)):
+    for batch_idx, (anchor_style, anchor_rgb, positive_style, positive_rgb, negative_style, negative_rgb) in enumerate(tqdm(loader)):
         torch.cuda.empty_cache()
 
-        if use_cuda:
-            anchor = anchor.to(device)
-            positive = positive.to(device)
-            negative = negative.to(device)
+        anchor_style = anchor_style.to(device)
+        anchor_rgb = anchor_rgb.to(device)
 
-        anchor = torch.cat((style_model(anchor).double(), content_model(anchor).double()), dim=1)
-        A = style_model(positive).double()
-        B = content_model(positive).double()
+        A = style_model(anchor_style).double()
+        B = rgb_model(anchor_rgb).double().to(device)
 
-        positive = torch.cat((A, B), dim=1)
 
-        A = style_model(negative).double()
-        B = content_model(negative).double()
-
-        negative = torch.cat((A, B), dim=1)
+        anchor = torch.cat((A, B), dim = 1)
+        positive = torch.cat((style_model(positive_style.to(device)).double(), rgb_model(positive_rgb.to(device)).double()), dim = 1)
+        negative = torch.cat((style_model(negative_style.to(device)).double(), rgb_model(negative_rgb.to(device)).double()), dim = 1)
 
 
         dis_pos = 1 - cos(anchor, positive)
@@ -187,7 +197,6 @@ for epoch in range(n_epochs):
 
         b = dis_pos.size()[0]
 
-        # losses = F.relu(dis_pos - dis_neg)
         losses = torch.max(torch.zeros(b).double().to(device), dis_pos - dis_neg)
 
 
@@ -195,14 +204,8 @@ for epoch in range(n_epochs):
         correct += count
         losses_sum = losses.sum()
         losses_mean = torch.mean(losses)
-        # print(losses_mean.item())
 
-
-        # style_optimizer.zero_grad()
-        # content_optimizer.zero_grad()
         style_model.zero_grad()
-        content_model.zero_grad()
-
 
         losses_mean.backward()
 
@@ -217,30 +220,21 @@ for epoch in range(n_epochs):
             for idx, param in enumerate(style_model.parameters()):
                 print(param)
 
-        for idx, param in enumerate(content_model.parameters()):
-            grad = param.grad.view(-1)
-            # print("content", idx, grad[:5])
-            if torch.isnan(grad).sum() > 0:
-                is_okay_to_optimize = False
-                print("content", idx, "grad nan")
-                break
-
         if is_okay_to_optimize:
             style_optimizer.step()
-            content_optimizer.step()
         else:
             break
 
     percent = 100. * correct / len(dataset)
     print("epoch: {}, correct: {}/{} ({:.0f}%)".format(epoch, correct, len(dataset), percent))
 
-save_path = './model_epoch100_Grapolio_유화_수채화_positive_and_negative_or.pth'
+
+save_path = './model_epoch50_BAM_ALL.pth'
 torch.save({
-            'style_state_dict': style_model.state_dict(),
-            'content_state_dict': content_model.state_dict(),
-            'optimizerA_state_dict': style_optimizer.state_dict(),
-            'optimizerB_state_dict': content_optimizer.state_dict(),
+            'model_state_dict': style_model.state_dict(),
+            'optimizer_state_dict': style_optimizer.state_dict(),
             }, save_path)
+
 
 
 
